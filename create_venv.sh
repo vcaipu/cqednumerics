c351d40ddca3d7050106bf29c4e296cb8e0cdbd3
Vincent Cai <vc9839@princeton.edu>
2025-12-06 13:45:29 -0500

    commit tests on poisson w dirichlet bcs

--- create_venv.sh
+++ create_venv.sh
@@ -5,4 +5,4 @@ pip install -r requirements.txt
 pip install jupyter ipykernel
 pip install jax
 pip install -U "jax[cuda13]"
-pip install matplotlib scikit-fem jaxopt
+pip install matplotlib scikit-fem jaxopt meshio

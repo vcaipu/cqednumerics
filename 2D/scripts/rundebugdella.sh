3230c8f25a823d1ad367297340761810d0d86f72
Vincent Cai <vc9839@princeton.edu>
2026-05-08 14:52:47 -0400

    della debugging

--- 2D/scripts/rundebugdella.sh
+++ 2D/scripts/rundebugdella.sh
@@ -1,17 +1,17 @@
-DIRNAME="debugdella2"
-N=70
+DIRNAME="debugdella9"
+N=100
 n=50
-SEPARATION=30
-SIDELLEN=20
+SEPARATION=20
+SIDELLEN=10
 
-LC_LARGE=5
-LC_SMALL=0.8
-ELEMENT_ORDER=2
-PADDING=25
+LC_LARGE=1.3
+LC_SMALL=1.3
+ELEMENT_ORDER=1
+PADDING=20
 GEOMETRY="rect"
-INNER_DIM=30
-FULL_LAMBDA_Y=False
-OPT_TOL=1e-4
+INNER_DIM=4
+OPT_TOL_OLD=5e-6
+OPT_TOL_NEW=5e-4 # Remember it is full Hamiltonian, vs old is per particle Hamiltonian
 OPT_MAXITER=1000
 
 # 1. Run OLD Script
@@ -23,6 +23,9 @@ python3 -u 2DZeroPoint.py \
     --separation=${SEPARATION} \
     --N=${N} \
     --n=${n} \
+    --opt_tol=${OPT_TOL_OLD} \
+    --opt_maxiter=${OPT_MAXITER} \
+
 
 # 2. Run NEW Script
 cd /scratch/gpfs/AROD/vc9839/finite-island-cqed/2D
@@ -40,6 +43,6 @@ python3 -u 2DTwoModesOpt.py \
     --lc_small=${LC_SMALL} \
     --lc_large=${LC_LARGE} \
     --element_order=${ELEMENT_ORDER} \
-    --opt_tol=${OPT_TOL} \
+    --opt_tol=${OPT_TOL_NEW} \
     --opt_maxiter=${OPT_MAXITER} \
-    --full_lambda_y=${FULL_LAMBDA_Y}
+    --no-full_lambda_y
